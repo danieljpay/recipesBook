6cec95a2e29a9cace218707334fe20ff44e09e77
Daniel Pérez <plupyt_dany@hotmail.com>
2020-10-02 12:25:22 -0500

    navigation in header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
   ],
   imports: [
     BrowserModule,
