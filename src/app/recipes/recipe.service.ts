ff4ee924355e579a0177c0f0e7cfdb39f2b8bd02
Daniel Pérez <plupyt_dany@hotmail.com>
2020-10-08 13:24:33 -0500

    ingredients in recipe-detail

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 import { Recipe } from './recipe.model';
 
@@ -6,10 +7,22 @@ export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 
-        'https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-5.jpg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 
-        'https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-5.jpg')
+        new Recipe(
+            'A Test Recipe', 
+            'This is simply a test', 
+            'https://www.afuegolento.com/img_db/timthumb.php?src=img_db/interviews/2019/01/interview-5c49cab09057f.jpg&w=800&z=1    ',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe(
+            'Another Test Recipe', 
+            'This is simply a test', 
+            'https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-5.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ])
     ];
 
     getRecipes() {
