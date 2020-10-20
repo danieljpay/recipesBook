d963ffbea3fcd633478f740f04702ee6e67d02fc
Daniel Pérez <plupyt_dany@hotmail.com>
2020-10-20 12:55:15 -0500

    editing shoppingEditComponent

--- src/app/shopping-list/shopping-list.services.ts
+++ src/app/shopping-list/shopping-list.services.ts
@@ -1,9 +1,9 @@
-import { Subject } from 'rxjs';
-
 import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs';
 
 export class ShoppingListService {
     ingredientsChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10),
@@ -11,6 +11,10 @@ export class ShoppingListService {
 
     getIngredients() {
         return this.ingredients.slice();
+    }
+
+    getIngredient(index: number) {
+        return this.ingredients[index];
     }
 
     addIngredient(ingredient: Ingredient) {
