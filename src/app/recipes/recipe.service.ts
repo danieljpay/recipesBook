80b3d4d80fd0961e21466387c65695248a131cb1
Daniel Pérez <plupyt_dany@hotmail.com>
2020-10-29 09:29:20 -0600

    fixed bugs

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,28 +9,34 @@ import { ShoppingListService } from '../shopping-list/shopping-list.services';
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'A Test Recipe', 
-            'This is simply a test', 
-            'https://www.afuegolento.com/img_db/timthumb.php?src=img_db/interviews/2019/01/interview-5c49cab09057f.jpg&w=800&z=1',
-            [
-                new Ingredient('Meat', 1),
-                new Ingredient('French Fries', 20)
-            ]
-        ),
-        new Recipe(
-            'Another Test Recipe', 
-            'This is simply a test', 
-            'https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-5.jpg',
-            [
-                new Ingredient('Buns', 2),
-                new Ingredient('Meat', 1)
-            ]
-        )
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'A Test Recipe', 
+    //         'This is simply a test', 
+    //         'https://www.afuegolento.com/img_db/timthumb.php?src=img_db/interviews/2019/01/interview-5c49cab09057f.jpg&w=800&z=1',
+    //         [
+    //             new Ingredient('Meat', 1),
+    //             new Ingredient('French Fries', 20)
+    //         ]
+    //     ),
+    //     new Recipe(
+    //         'Another Test Recipe', 
+    //         'This is simply a test', 
+    //         'https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-5.jpg',
+    //         [
+    //             new Ingredient('Buns', 2),
+    //             new Ingredient('Meat', 1)
+    //         ]
+    //     )
+    // ];
+    private recipes: Recipe[] = [];
 
     constructor(private slService: ShoppingListService) {}
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
 
     getRecipes() {
         return this.recipes.slice();
